ff9524de58651b9512c91cc1cdf25d29eabc26ce
wonder-mice <wonder.mice@gmail.com>
2015-04-30 10:53:00 -0700

    Test arguments evaluation

--- tests/log_level_tests.c
+++ tests/log_level_tests.c
@@ -4,15 +4,17 @@
 #define ZF_LOG_LEVEL ZF_LOG_INFO
 #include <zf_log.h>
 
-static bool g_output;
+static unsigned g_output;
 static int g_lvl;
 static char g_msg[1024];
 static unsigned g_len;
+static unsigned g_arg;
 
 static void reset()
 {
-	g_output = false;
+	g_output = 0;
 	zf_log_set_output_level(0);
+	g_arg = 0;
 }
 
 static void output_callback(int lvl, char *s, unsigned len)
@@ -20,18 +22,24 @@ static void output_callback(int lvl, char *s, unsigned len)
 	g_lvl = lvl;
 	strcpy(g_msg, s);
 	g_len = len;
-	g_output = true;
+	++g_output;
+}
+
+static int get_arg()
+{
+	++g_arg;
+	return g_arg;
 }
 
 static void test_current_level()
 {
 	reset();
 	ZF_LOGD("no debug log");
-	TEST_VERIFY_FALSE(g_output);
+	TEST_VERIFY_EQUAL(g_output, 0);
 
 	reset();
 	ZF_LOGI("info log");
-	TEST_VERIFY_TRUE(g_output);
+	TEST_VERIFY_EQUAL(g_output, 1);
 	TEST_VERIFY_EQUAL(g_lvl, ZF_LOG_INFO);
 }
 
@@ -40,33 +48,49 @@ static void test_output_level()
 	reset();
 	zf_log_set_output_level(ZF_LOG_WARN);
 	ZF_LOGD("no debug log");
-	TEST_VERIFY_FALSE(g_output);
+	TEST_VERIFY_EQUAL(g_output, 0);
 	ZF_LOGI("no info log");
-	TEST_VERIFY_FALSE(g_output);
+	TEST_VERIFY_EQUAL(g_output, 0);
 
 	reset();
 	zf_log_set_output_level(ZF_LOG_INFO);
 	ZF_LOGD("no debug log");
-	TEST_VERIFY_FALSE(g_output);
+	TEST_VERIFY_EQUAL(g_output, 0);
 	ZF_LOGI("info log");
-	TEST_VERIFY_TRUE(g_output);
+	TEST_VERIFY_EQUAL(g_output, 1);
 	TEST_VERIFY_EQUAL(g_lvl, ZF_LOG_INFO);
 
 	reset();
 	zf_log_set_output_level(ZF_LOG_DEBUG);
 	ZF_LOGD("no debug log");
-	TEST_VERIFY_FALSE(g_output);
+	TEST_VERIFY_EQUAL(g_output, 0);
 	ZF_LOGI("info log");
-	TEST_VERIFY_TRUE(g_output);
+	TEST_VERIFY_EQUAL(g_output, 1);
 	TEST_VERIFY_EQUAL(g_lvl, ZF_LOG_INFO);
 
 	reset();
 	zf_log_set_output_level(0);
 	ZF_LOGD("no debug log");
-	TEST_VERIFY_FALSE(g_output);
+	TEST_VERIFY_EQUAL(g_output, 0);
 	ZF_LOGI("info log");
-	TEST_VERIFY_TRUE(g_output);
+	TEST_VERIFY_EQUAL(g_output, 1);
 	TEST_VERIFY_EQUAL(g_lvl, ZF_LOG_INFO);
+}
+
+static void test_args_evaluation()
+{
+	reset();
+	ZF_LOGD("debug log arg not evaluated: %i", get_arg());
+	TEST_VERIFY_EQUAL(g_arg, 0);
+
+	reset();
+	ZF_LOGI("info log arg evaluated: %i", get_arg());
+	TEST_VERIFY_EQUAL(g_arg, 1);
+
+	reset();
+	zf_log_set_output_level(ZF_LOG_WARN);
+	ZF_LOGI("info log arg not evaluated: %i", get_arg());
+	TEST_VERIFY_EQUAL(g_arg, 0);
 }
 
 int main(int argc, char *argv[])
@@ -76,6 +100,7 @@ int main(int argc, char *argv[])
 
 	TEST_EXECUTE(test_current_level());
 	TEST_EXECUTE(test_output_level());
+	TEST_EXECUTE(test_args_evaluation());
 
 	return TEST_RUNNER_EXIT_CODE();
 }
