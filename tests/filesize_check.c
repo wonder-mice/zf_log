a6ece7cc8082b7d7cb6a993170ed17c01d51bebd
wonder-mice <wonder.mice@gmail.com>
2015-05-03 00:33:47 -0700

    Add tests for generated code size

---
+++ tests/filesize_check.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static long filesize(const char *const path)
+{
+	FILE *const f = fopen(path, "rb");
+	if (0 == f)
+	{
+		fprintf(stderr, "Bad file: %s\n", path);
+		exit(1);
+	}
+	fseek(f, 0, SEEK_END);
+	const long sz = (unsigned)ftell(f);
+	fclose(f);
+	return sz;
+}
+
+int main(int argc, const char *argv[])
+{
+	if (2 >= argc)
+	{
+		fprintf(stderr, "Usage: prog f1_path f2_path\n");
+		return 1;
+	}
+	const long f1_sz = filesize(argv[1]);
+	const long f2_sz = filesize(argv[2]);
+	if (f1_sz < f2_sz)
+	{
+		fprintf(stderr, "New size is larger: %li <  %li\n", f1_sz, f2_sz);
+		return 1;
+	}
+	return 0;
+}
