2525d61ce8d5777b49cb856d08be6f6730be5ace
wonder-mice <wonder.mice@gmail.com>
2016-01-04 17:20:23 -0800

    Add C++ compilation tests

---
+++ tests/cpp_compilation_tests.cpp
@@ -0,0 +1,16 @@
+#include <zf_log.c>
+
+namespace
+{
+	void mock_output_callback(zf_log_output_ctx *)
+	{
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	zf_log_set_output_callback(ZF_LOG_PUT_STD, mock_output_callback);
+	ZF_LOGI("log from cpp, argc=%i", argc);
+	ZF_LOGI_MEM(argv, argc * sizeof(*argv), "log from cpp, argv pointers:");
+	return 0;
+}
---
+++ tests/cpp_extern_definition_tests.cpp
@@ -0,0 +1,25 @@
+#define ZF_LOG_EXTERN_TAG_PREFIX
+#define ZF_LOG_EXTERN_GLOBAL_FORMAT
+#define ZF_LOG_EXTERN_GLOBAL_OUTPUT
+#define ZF_LOG_EXTERN_GLOBAL_OUTPUT_LEVEL
+#include <zf_log.c>
+
+ZF_LOG_DEFINE_TAG_PREFIX = "MAIN";
+ZF_LOG_DEFINE_GLOBAL_FORMAT = {32};
+ZF_LOG_DEFINE_GLOBAL_OUTPUT = ZF_LOG_OUT_STDERR;
+ZF_LOG_DEFINE_GLOBAL_OUTPUT_LEVEL = ZF_LOG_INFO;
+
+namespace
+{
+	void mock_output_callback(zf_log_output_ctx *)
+	{
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	zf_log_set_output_callback(ZF_LOG_PUT_STD, mock_output_callback);
+	ZF_LOGI("log from cpp, argc=%i", argc);
+	ZF_LOGI_MEM(argv, argc * sizeof(*argv), "log from cpp, argv pointers:");
+	return 0;
+}
