8a9f478d6e307420a95121821f9d37e23b00327a
wonder-mice <wonder.mice@gmail.com>
2016-01-19 01:14:27 -0800

    Add size tests for major logging libraries

--- tests/perf/library_switch.h
+++ tests/perf/library_switch.h
@@ -2,6 +2,9 @@
 
 #define TEST_LIBRARY_ID_zf_log 1
 #define TEST_LIBRARY_ID_spdlog 2
+#define TEST_LIBRARY_ID_easylog 3
+#define TEST_LIBRARY_ID_g3log 4
+#define TEST_LIBRARY_ID_glog 5
 
 #define _CONCAT(a, b) a##b
 #define CONCAT(a, b) _CONCAT(a, b)
@@ -10,6 +13,12 @@
 	#define TEST_LIBRARY_ZF_LOG
 #elif TEST_LIBRARY_ID_spdlog == CONCAT(TEST_LIBRARY_ID_, TEST_LIBRARY)
 	#define TEST_LIBRARY_SPDLOG
+#elif TEST_LIBRARY_ID_easylog == CONCAT(TEST_LIBRARY_ID_, TEST_LIBRARY)
+	#define TEST_LIBRARY_EASYLOG
+#elif TEST_LIBRARY_ID_g3log == CONCAT(TEST_LIBRARY_ID_, TEST_LIBRARY)
+	#define TEST_LIBRARY_G3LOG
+#elif TEST_LIBRARY_ID_glog == CONCAT(TEST_LIBRARY_ID_, TEST_LIBRARY)
+	#define TEST_LIBRARY_GLOG
 #else
 	#error Unknown test library name
 #endif
--- tests/perf/run_tests.py
+++ tests/perf/run_tests.py
@@ -3,34 +3,99 @@
 import sys
 import os
 import argparse
-import pprint
+import json
+
+class data_bytes:
+	def __init__(self, value):
+		if type(value) is not int:
+			raise RuntimeError("Not an int")
+		self.value = value
+	def __str__(self):
+		if self.value < 1024:
+			return "%ib " % (self.value)
+		if self.value < 1024 * 1024:
+			return "%.2fkb" % (self.value / 1024.0)
+		return "%.2fmb" % (self.value / 1024.0 / 1024.0)
+	def __repr__(self):
+		return repr(self.value)
+
+def to_table_str(value, w=0):
+	if value is None:
+		return "-".center(w)
+	elif type(value) is int:
+		return '{0:\'}'.format(value).rjust(w)
+	elif type(value) is str:
+		return value.center(w)
+	elif isinstance(value, data_bytes):
+		return str(value).rjust(w)
+	else:
+		raise RuntimeError("Type not supported: " + str(type(value)))
 
 def run_call_site_size(params):
 	ps = params["call_site_size"]
 	result = dict()
-	pprint.pprint(ps)
-	for subj in ps.keys():
-		print "subj", subj
+	for subj in ps:
 		sz1 = os.path.getsize(ps[subj]["1"])
 		sz2 = os.path.getsize(ps[subj]["2"])
-		result[subj] = sz2 - sz1
+		result[subj] = data_bytes(sz2 - sz1)
 	return result
 
 def run_min_executable_size(params):
 	ps = params["min_executable_size"]
 	result = dict()
-	pprint.pprint(ps)
 	for subj in ps.keys():
-		print "subj", subj
 		sz = os.path.getsize(ps[subj])
-		result[subj] = sz
+		result[subj] = data_bytes(sz)
 	return result
 
-def run_tests(out, params):
+def run_tests(params):
 	result = dict()
 	result["call_site_size"] = run_call_site_size(params)
 	result["min_executable_size"] = run_min_executable_size(params)
-	pprint.pprint(result, out)
+	return result
+
+def write_table(result, out):
+	tests = result.keys()
+	subjs = set()
+	# collect all subjects
+	for test in tests:
+		subjs.update(result[test].keys())
+	subjs = sorted(list(subjs))
+	# create table
+	rows = len(tests) + 1
+	cols = len(subjs) + 1
+	table = [[None for _ in range(cols)] for _ in range(rows)]
+	# put names and captions
+	for i in range(1, rows):
+		table[i][0] = tests[i - 1]
+	for j in range(1, cols):
+		table[0][j] = subjs[j - 1]
+	# put data
+	for i in range(1, rows):
+		for j in range(1, cols):
+			test = tests[i - 1]
+			subj = subjs[j - 1]
+			if subj in result[test]:
+				table[i][j] = result[test][subj]
+	# find cols width
+	widths = [0 for _ in range(cols)]
+	for j in range(0, cols):
+		for i in range(0, rows):
+			s = to_table_str(table[i][j])
+			if widths[j] < len(s):
+				widths[j] = len(s)
+	# draw chart
+	margins = 2
+	line = "+" + "-" * (sum(widths) + margins * len(widths) + len(widths) - 1) + "+"
+	chart = line + "\n"
+	for i in range(0, rows):
+		chart += "|"
+		for j in range(0, cols):
+			w = widths[j]
+			s = to_table_str(table[i][j], w)
+			chart += s.center(w + margins) + "|"
+		chart += "\n" + line + "\n"
+	out.write(chart)
 
 def main(argv):
 	parser = argparse.ArgumentParser()
@@ -41,6 +106,7 @@ def main(argv):
 	parser.add_argument("-b", "--build", metavar="TYPE",
 			help="Input parameter")
 	args = parser.parse_args(argv[1:])
+	# process parameters
 	params = dict()
 	for p in args.parameter:
 		d = params
@@ -55,10 +121,15 @@ def main(argv):
 			key = vs[i]
 			if key not in d:
 				d[key] = dict()
+	# run tests
+	result = run_tests(params)
+	# print results
 	out = sys.stdout
 	if args.out is not None:
 		out = open(args.out, 'w')
-	run_tests(out, params)
+	out.write(json.dumps(result, indent=4, default=repr))
+	out.write("\n")
+	write_table(result, out)
 	out.flush()
 	return 0
 
