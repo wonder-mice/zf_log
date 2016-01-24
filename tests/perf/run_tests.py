df5406c63750cbbbdc72f4caa0777766dc9b0550
wonder-mice <wonder.mice@gmail.com>
2016-01-24 00:48:27 -0800

    Markdown output support for perf tests

--- tests/perf/run_tests.py
+++ tests/perf/run_tests.py
@@ -5,6 +5,7 @@ import os
 import argparse
 import subprocess
 import json
+import pprint
 
 def take_first(value):
 	if type(value) is tuple or type(value) is list:
@@ -24,6 +25,25 @@ def take_map(value, keys, vals):
 		if value == keys[i]:
 			return vals[i]
 	raise RuntimeError("Value \"%s\" is not in \"%s\"" % value, keys)
+
+def take_best(values, key, compare, reverse=False):
+	if 0 == len(values):
+		return []
+	values = sorted(values, key=key, reverse=reverse)
+	for i in range(1, len(values)):
+		if not compare(values[0], values[i]):
+			return values[0:i]
+	return values
+
+def _cmp_percentage(p, key, a, b):
+	if key(a) == key(b):
+		return True
+	if p >= abs((key(a) - key(b)) / float(key(a))):
+		return True
+	return False
+
+def cmp_percentage(p, key):
+	return lambda a, b: _cmp_percentage(p, key, a, b)
 
 def translate_test(test):
 	name = take_first(test)
@@ -62,7 +82,27 @@ def translation_sort_key(v):
 def translation_value(v):
 	return v[1]
 
-class data_bytes:
+class data_cell:
+	def __init__(self):
+		self.best = False
+	def set_best(self, best=True):
+		self.best = best
+	def ifbest(self, a, b):
+		if hasattr(self, "best") and self.best:
+			return a
+		return b
+
+class data_str(data_cell):
+	def __init__(self, value):
+		if type(value) is not str:
+			raise RuntimeError("Not a string")
+		self.value = value
+	def __str__(self):
+		return self.value
+	def __repr__(self):
+		return repr(self.value)
+
+class data_bytes(data_cell):
 	def __init__(self, value):
 		if type(value) is not int:
 			raise RuntimeError("Not an int")
@@ -71,12 +111,12 @@ class data_bytes:
 		if self.value < 1024:
 			return "%i B" % (self.value)
 		if self.value < 1024 * 1024:
-			return "%.2f KiB" % (self.value / 1024.0)
-		return "%.2f MiB" % (self.value / 1024.0 / 1024.0)
+			return "%.2f KB" % (self.value / 1024.0)
+		return "%.2f MB" % (self.value / 1024.0 / 1024.0)
 	def __repr__(self):
 		return repr(self.value)
 
-class data_seconds:
+class data_seconds(data_cell):
 	def __init__(self, value):
 		if type(value) is not int and type(value) is not float:
 			raise RuntimeError("Not an int or float")
@@ -86,7 +126,7 @@ class data_seconds:
 	def __repr__(self):
 		return repr(self.value)
 
-class data_freq:
+class data_freq(data_cell):
 	def __init__(self, count, seconds):
 		if type(count) is not int and type(count) is not float:
 			raise RuntimeError("Not an int or float")
@@ -98,6 +138,8 @@ class data_freq:
 		return "{:,}".format(self.count / self.seconds)
 	def __repr__(self):
 		return repr((self.count, self.seconds))
+	def freq(self):
+		return self.count / self.seconds
 
 def get_table_data(result):
 	# collect all tests
@@ -114,9 +156,9 @@ def get_table_data(result):
 	table = [[None for _ in range(cols)] for _ in range(rows)]
 	# put names and captions
 	for i in range(1, rows):
-		table[i][0] = translation_value(translate_test(tests[i - 1]))
+		table[i][0] = data_str(translation_value(translate_test(tests[i - 1])))
 	for j in range(1, cols):
-		table[0][j] = translation_value(translate_subj(subjs[j - 1]))
+		table[0][j] = data_str(translation_value(translate_subj(subjs[j - 1])))
 	# put data
 	for i in range(1, rows):
 		for j in range(1, cols):
@@ -124,45 +166,81 @@ def get_table_data(result):
 			subj = subjs[j - 1]
 			if subj in result[test]:
 				table[i][j] = result[test][subj]
-	return table, rows, cols
-
-def gen_table_ascii(result):
-	table, rows, cols = get_table_data(result)
 	# gen cells content
 	for i in range(0, rows):
 		for j in range(0, cols):
 			value = table[i][j]
 			if value is None:
-				value = "-"
-			elif type(value) is int:
-				value = "{:,}".format(value)
-			elif type(value) is not str:
-				value = str(value)
+				value = data_str("")
+			elif not isinstance(value, data_cell):
+				raise RuntimeError("Value \"%s\" is of unsupported type \"%s\"" % (value, type(value)))
 			table[i][j] = value
 	# find cols width
 	widths = [0 for _ in range(cols)]
 	for j in range(0, cols):
 		for i in range(0, rows):
-			s = table[i][j]
+			s = str(table[i][j])
 			if widths[j] < len(s):
 				widths[j] = len(s)
-	# align cells
+	return table, rows, cols, widths
+
+def gen_table_ascii(result):
+	table, rows, cols, widths = get_table_data(result)
+	# apply cell format
+	margin_norm  = (" ", " ")
+	margin_best  = ("*", " ")
+	margins = max(map(len, margin_norm)) + max(map(len, margin_norm))
 	for i in range(1, rows):
-		table[i][0] = table[i][0].ljust(widths[0])
+		table[i][0] = str(table[i][0]).ljust(widths[0]).join(margin_norm)
 	for j in range(0, cols):
-		table[0][j] = table[0][j].center(widths[j])
+		table[0][j] = str(table[0][j]).center(widths[j]).join(margin_norm)
 	for i in range(1, rows):
 		for j in range(1, cols):
-			table[i][j] = table[i][j].rjust(widths[j])
+			data = table[i][j]
+			margin = data.ifbest(margin_best, margin_norm)
+			table[i][j] = str(data).rjust(widths[j]).join(margin)
 	# draw chart
-	margin = " "
-	line = "+" + "-" * (sum(widths) + (2 * len(margin) + 1) * len(widths) - 1) + "+\n"
+	line = "+" + "-" * (sum(widths) + (margins + 1) * len(widths) - 1) + "+\n"
 	chart = line
 	for row in table:
 		chart += "|"
 		for cell in row:
-			chart += "%s%s%s|" % (margin, cell, margin)
+			chart += "%s|" % (cell)
 		chart += "\n" + line
+	return chart
+
+def gen_table_markdown(result):
+	table, rows, cols, widths = get_table_data(result)
+	# apply cell format
+	margin_norm  = ("  ", "  ")
+	margin_best  = ("**", "**")
+	margins = max(map(len, margin_norm)) + max(map(len, margin_norm))
+	for i in range(1, rows):
+		table[i][0] = str(table[i][0]).ljust(widths[0]).join(margin_norm)
+	for j in range(0, cols):
+		table[0][j] = str(table[0][j]).center(widths[j]).join(margin_norm)
+	for i in range(1, rows):
+		for j in range(1, cols):
+			data = table[i][j]
+			margin = data.ifbest(margin_best, margin_norm)
+			table[i][j] = str(data).join(margin).rjust(widths[j] + margins)
+	# draw chart
+	chart = ""
+	if 0 == rows:
+		return chart
+	chart += "|"
+	for cell in table[0]:
+		chart += "%s|" % (cell)
+	chart += "\n"
+	chart += "| " + "-" * (margins + widths[0] - 2) + " |"
+	for i in range(1, cols):
+		chart += " " + "-" * (margins + widths[i] - 2) + ":|"
+	chart += "\n"
+	for i in range(1, rows):
+		chart += "|"
+		for j in range(0, cols):
+			chart += "%s|" % (table[i][j])
+		chart += "\n"
 	return chart
 
 def run_call_site_size(params, result):
@@ -177,8 +255,13 @@ def run_call_site_size(params, result):
 			data = params[mode][subj]
 			sz1 = os.path.getsize(data["1"])
 			sz2 = os.path.getsize(data["2"])
-			values[subj] = data_bytes(sz2 - sz1)
+			data = data_bytes(sz2 - sz1)
+			values[subj] = data
 		result[name] = values
+		for best in take_best(values.values(),
+							  key=lambda x: x.value,
+							  compare=cmp_percentage(0.0, key=lambda x: x.value)):
+			best.set_best()
 
 def run_executable_size(params, result):
 	if type(result) is not dict:
@@ -192,6 +275,10 @@ def run_executable_size(params, result):
 			sz = os.path.getsize(params[mode][subj])
 			values[subj] = data_bytes(sz)
 		result[name] = values
+		for best in take_best(values.values(),
+							  key=lambda x: x.value,
+							  compare=cmp_percentage(0.0, key=lambda x: x.value)):
+			best.set_best()
 
 def run_build_time(params, result, id):
 	if type(result) is not dict:
@@ -203,6 +290,10 @@ def run_build_time(params, result, id):
 			dt = json.load(f)["dt"]
 			values[subj] = data_seconds(dt)
 	result[id] = values
+	for best in take_best(values.values(),
+						  key=lambda x: x.value,
+						  compare=cmp_percentage(0.2, key=lambda x: x.value)):
+		best.set_best()
 
 def run_speed(params, result, threads, seconds=1):
 	if type(result) is not dict:
@@ -218,6 +309,10 @@ def run_speed(params, result, threads, seconds=1):
 			stdout, stderr = p.communicate()
 			values[subj] = data_freq(int(stdout), seconds)
 		result[name] = values
+		for best in take_best(values.values(),
+							  key=lambda x: x.freq(), reverse=True,
+							  compare=cmp_percentage(0.1, key=lambda x: x.freq())):
+			best.set_best()
 
 def run_tests(params):
 	result = dict()
@@ -231,8 +326,10 @@ def run_tests(params):
 
 def main(argv):
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-o", "--out", metavar="PATH",
-			help="Output file path")
+	parser.add_argument("-t", "--text", metavar="PATH", default=None,
+			help="Text output file path")
+	parser.add_argument("-m", "--markdown", metavar="PATH", default=None,
+			help="Markdown output file path")
 	parser.add_argument("-p", "--parameter", metavar="VALUE", action="append", default=[],
 			help="Input parameter")
 	parser.add_argument("-b", "--build", metavar="TYPE",
@@ -255,19 +352,22 @@ def main(argv):
 			key = vs[i]
 			if key not in d:
 				d[key] = dict()
-	out = sys.stdout
-	if args.out is not None:
-		out = open(args.out, 'w')
 	if args.verbose:
-		out.write(json.dumps(params, indent=4, default=repr))
-		out.write("\n")
+		sys.stderr.write(pprint.pformat(params, indent=4))
+		sys.stderr.write("\n")
+	# run, run, run!
 	result = run_tests(params)
 	if args.verbose:
-		out.write(json.dumps(result, indent=4, default=repr))
-		out.write("\n")
-	table = gen_table_ascii(result)
-	out.write(table)
-	out.flush()
+		sys.stderr.write(pprint.pformat(result, indent=4))
+		sys.stderr.write("\n")
+	if args.text is not None:
+		with open(args.text, "w") as f:
+			table = gen_table_ascii(result)
+			f.write(table)
+	if args.markdown is not None:
+		with open(args.markdown, "w") as f:
+			table = gen_table_markdown(result)
+			f.write(table)
 	return 0
 
 if __name__ == "__main__":
