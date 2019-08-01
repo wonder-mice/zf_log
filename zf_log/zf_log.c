e88bc100b9b993ea23d0b8b6c287bf713378df62
Andrey Pokrovskiy <apk@apple.com>
2019-08-01 17:24:07 +0300

    Failed to build under cygwin #37

--- zf_log/zf_log.c
+++ zf_log/zf_log.c
@@ -309,10 +309,8 @@
 	#include <sys/time.h>
 	#if defined(__linux__)
 		#include <linux/limits.h>
-	#elif defined(_AIX)
-		#include <limits.h>
 	#else
-		#include <sys/syslimits.h>
+		#include <limits.h>
 	#endif
 #endif
 
